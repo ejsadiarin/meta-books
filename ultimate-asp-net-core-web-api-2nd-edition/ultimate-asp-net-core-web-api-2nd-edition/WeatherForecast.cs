80717ac1f94f6908c05165d5437af805c703a8ac
Ej Sadiarin <ejsadiarin@gmail.com>
2023-06-30 22:06:10 +0800

    initial commit, 24 grind this 394 page book fok it

---
+++ ultimate-asp-net-core-web-api-2nd-edition/ultimate-asp-net-core-web-api-2nd-edition/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace ultimate_asp_net_core_web_api_2nd_edition;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
