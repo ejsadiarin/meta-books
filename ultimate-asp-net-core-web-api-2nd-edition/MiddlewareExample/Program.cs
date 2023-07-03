6b36fb89547b58a08ec8b779ec9a3df322798147
Ej Sadiarin <ejsadiarin@gmail.com>
2023-07-03 11:18:39 +0800

    initialize logging service and contracts

--- ultimate-asp-net-core-web-api-2nd-edition/MiddlewareExample/Program.cs
+++ ultimate-asp-net-core-web-api-2nd-edition/MiddlewareExample/Program.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,6 +21,42 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+    await next.Invoke();
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+});
+
+app.Map("/usingmapbranch", builder =>
+{
+    builder.Use(async (context, next) =>
+    {
+        Console.WriteLine($"Map branch logic in the Use method before the next delegate");
+        await next.Invoke();
+        Console.WriteLine($"Map branch logic in the Use method after the next delegate");
+    });
+    builder.Run(async context =>
+    {
+        Console.WriteLine($"Map branch response to the client in the Run method");
+        await context.Response.WriteAsync("MA MAN from the map branch");
+    });
+});
+
+app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder =>
+{
+    builder.Run(async context =>
+    {
+        await context.Response.WriteAsync("YO from MapWhen branch");
+    });
+});
+
+app.Run(async context =>
+{
+    Console.WriteLine($"Writing the response to the client in the Run method...");
+    await context.Response.WriteAsync("From middleware component message");
+});
 
 app.MapControllers();
 
